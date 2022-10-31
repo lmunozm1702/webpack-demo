edbac7c503419ea1918e6c366fa9ea6dfde9c3e8
Luis Muñoz <lmunozm1702@gmail.com>
2022-10-31 15:21:54 -0300

    finish setup
    Configure:
    - html-webpack
    - web-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack 1st. Installation',
+      template: './src/index.html',
+    }),
+  ],
   output: {
-    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
+    filename: "dist/[name].js",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
 };
